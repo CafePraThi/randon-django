2745c4dabe43d2a446e145513dfcf8180d19132e
Thiago Cardoso <cafeprathi@gmail.com>
2024-04-23 09:19:41 -0300

    static and base

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from blog.data import posts
 # Create your views here.
+
+def blog(request):
+  print('blog')
+
+  context = {
+    'texto': 'Bem vindo ao Blog',
+    'posts': posts
+  }
+
+  return render(request, 'blog/index.html', context)
--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def home(request):
+  print('home')
+
+  context={'text': 'asdasd',
+           'title': 'Home',
+           }
+
+  return render(request, 'home/index.html', context)
