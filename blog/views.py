7b294104856d587605a46e6a61e605c1a6dc1613
Thiago Cardoso <cafeprathi@gmail.com>
2024-04-24 16:53:45 -0300

    404

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from blog.data import posts
+from typing import Any
+from django.http import HttpRequest
 # Create your views here.
 
 def blog(request):
@@ -12,3 +14,22 @@ def blog(request):
   }
 
   return render(request, 'blog/index.html', context)
+
+def post(request: HttpRequest, post_id: int):
+  found_post: dict[str, Any] | None = None
+
+  for post in posts:
+    if post['id'] == post_id:
+      found_post = post
+      break
+
+  if found_post is None:
+    raise Http404('Post Não existe.')
+
+  context = {
+    #'texto': 'Bem vindo ao Blog',
+    'post': found_post,
+    'title': found_post['title']
+    
+  }
+  return render(request, 'blog/post.html', context)
